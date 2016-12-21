03ddb24fb02b4f282363109ee388d68db984d253
Andrew Keller <andrew.keller.09@gmail.com>
2016-12-20 20:46:31 -0800

    Add some methods for NoRender

--- src/paths/contstyles/norender.jl
+++ src/paths/contstyles/norender.jl
@@ -5,3 +5,10 @@ type NoRender{T} <: ContinuousStyle{T} end
 """
 type NoRender{T} <: ContinuousStyle{T} end
 NoRender() = NoRender{Float64}()
+copy(x::NoRender) = NoRender()
+
+distance{T}(s::NoRender{T}, t) = zero(T)
+extent{T}(s::NoRender{T}, t) = zero(T)
+paths(::NoRender, t...) = 0
+width{T}(s::NoRender{T}, t) = zero(T)
+divs(s::NoRender) = [0.0, 1.0]
