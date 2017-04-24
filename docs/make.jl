887d61e45951d4e6eaa86ab4377c667d99211a82
Andrew Keller <andrew.keller.09@gmail.com>
2017-04-23 22:48:56 -0700

    Tweak documenter settings

--- docs/make.jl
+++ docs/make.jl
@@ -5,7 +5,7 @@ makedocs()
 
 deploydocs(
     deps   = Deps.pip("mkdocs", "mkdocs-material", "python-markdown-math"),
-    julia  = "0.6",
+    julia  = "0.5",
     osname = "linux",
     repo   = "github.com/PainterQubits/Devices.jl.git"
 )
