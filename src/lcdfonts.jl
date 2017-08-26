6de90f53f17878b3bfbbb60dc16e0f95568c1f9a
Andrew Keller <ajkeller34@users.noreply.github.com>
2017-08-26 09:09:58 -0700

    Fix docstring

--- src/lcdfonts.jl
+++ src/lcdfonts.jl
@@ -327,7 +327,8 @@ function characters_demo(save_path = joinpath(homedir(),"Desktop","characters.gd
 end
 
 """
-    characters_demo(save_path = joinpath(homedir(),"Desktop","referenced_characters.gds"))
+    referenced_characters_demo(save_path = joinpath(homedir(),"Desktop","referenced_characters.gds");
+        verbose_override = false)
 Demo script for demonstrating the memory saving ability of keeping CellReferences for
 previously used characters in `lcdstring!()`. Nothing is printed if `verbose_override` is `true`.
 """
