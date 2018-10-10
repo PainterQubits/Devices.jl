9f17ca47d595aa290d5806c66140c638a3ac0389
Andrew Keller <andrew.keller.09@gmail.com>
2018-10-10 15:21:13 -0700

    Try to fix pip version resolution when building docs.

--- docs/make.jl
+++ docs/make.jl
@@ -4,7 +4,7 @@ using FileIO
 makedocs()
 
 deploydocs(
-    deps   = Deps.pip("mkdocs", "mkdocs-material", "python-markdown-math"),
+    deps   = Deps.pip("mkdocs==0.17.5", "mkdocs-material==2.9.4" ,"python-markdown-math"),
     julia  = "0.7",
     osname = "linux",
     repo   = "github.com/PainterQubits/Devices.jl.git"
