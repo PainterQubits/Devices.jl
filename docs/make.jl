41436f4e5309274280dd24b42d803d6f34eaa04d
Andrew Keller <andrew.keller.09@gmail.com>
2018-11-02 11:05:52 -0700

    Doc build process patch

--- docs/make.jl
+++ docs/make.jl
@@ -2,10 +2,9 @@ using Documenter, Devices
 using FileIO
 
 makedocs()
-
 deploydocs(
-    deps   = Deps.pip("mkdocs==0.17.5", "mkdocs-material==2.9.4" ,"python-markdown-math"),
-    julia  = "0.7",
+    deps   = Deps.pip("Tornado>=4.0.0,<5.0.0", "mkdocs==0.17.5", "mkdocs-material==2.9.4", "python-markdown-math"),
+    julia  = "1.0",
     osname = "linux",
     repo   = "github.com/PainterQubits/Devices.jl.git"
 )
