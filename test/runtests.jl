PainterQubits/Devices.jl

commit b084d381ca909165b3d42e922b7ef700d52a295b

Require that `extent` always takes a path length argument, never an argument in [0,1].
Modify taper methods accordingly.
Introduce `ContinuousStyle{CanStretch}` for concrete taper styles and use it in `reconcilefields!`.

src/paths/paths.jl
@@ -61,15 +61,16 @@ export reconcile!,
 
 """
     extent(s,t)
-For a style `s` and parameteric argument `t`, returns a distance tangential
-to the path specifying the lateral extent of the polygons rendered.
+For a style `s`, returns a distance tangential to the path specifying the lateral extent
+of the polygons rendered. The extent is measured from the center of the path to the edge
+of the polygon (half the total width along the path). The extent is evaluated at path length
+`t` from the start of the associated segment.
 """
 function extent end
 
 """
     width(s,t)
-For a style `s` and parameteric argument `t`, returns the width
-of paths rendered.
+For a style `s` and parameteric argument `t`, returns the width of paths rendered.
 """
 function width end
 
@@ -80,10 +81,15 @@ How to render a given path segment.
 abstract type Style end
 
 """
-    abstract type ContinuousStyle <: Style end
-Any style that applies to segments which have non-zero path length.
+    abstract type ContinuousStyle{CanStretch} <: Style end
+Any style that applies to segments which have non-zero path length. For most styles,
+`CanStretch == false`. An example of an exception is a linear taper, e.g.
+[`Paths.TaperTrace`](@ref), where you fix the starting and ending trace widths and let the
+segment length dictate the abruptness of the transition (hence, stretching the style).
+Concrete types inheriting from `ContinuousStyle{true}` should have a length field as the
+last field of their structure.
 """
-abstract type ContinuousStyle <: Style end
+abstract type ContinuousStyle{CanStretch} <: Style end
 
 """
     abstract type DiscreteStyle <: Style end
@@ -91,18 +97,10 @@ Any style that applies to segments which have zero path length.
 """
 abstract type DiscreteStyle <: Style end
 
-#=
-For some styles (e.g. tapers) we will want to provide a segment length to the style during
-operations like `straight!(path, L, TaperCPW(...))` in order to concretely specify how the
-geometry will look. Otherwise the geometry is only partially specified, which makes
-operations like `extent(s, t)` impossible to be used generically.
-=#
-_lengthhint(s::Style) = s
-
 """
     abstract type Segment{T<:Coordinate} end
-Path segment in the plane. All Segment objects should have the implement
-the following methods:
+Path segment in the plane. All Segment objects should have the implement the following
+methods:
 
 - `pathlength`
 - `p0`
@@ -118,17 +116,16 @@ abstract type Segment{T<:Coordinate} end
 Base.zero(::Segment{T}) where {T} = zero(T)     # TODO: remove and fix for 0.6 only versions
 Base.zero(::Type{Segment{T}}) where {T} = zero(T)
 
+# Used only to get dispatch to work right with ForwardDiff.jl.
+struct Curv{T} s::T end
+(s::Curv)(t) = ForwardDiff.derivative(s.s,t)
+
 """
     curvature(s, t)
 Returns the curvature of a function `t->Point(x(t),y(t))` at `t`. The result will have units
 of inverse length if units were used for the segment. The result can be interpreted as the
 inverse radius of a circle with the same curvature.
 """
-curvature
-
-# Used only to get dispatch to work right with ForwardDiff.jl.
-struct Curv{T} s::T end
-(s::Curv)(t) = ForwardDiff.derivative(s.s,t)
 curvature(s, t) = ForwardDiff.derivative(Curv(s), t)
 
 abstract type DiscreteSegment{T} <: Segment{T} end
@@ -152,10 +149,11 @@ mutable struct Node{T<:Coordinate}
 end
 
 """
-    Node{T}(a::Segment{T}, b::Style)
+    Node(a::Segment{T}, b::Style) where {T}
 Create a node with segment `a` and style `b`.
 """
 Node(a::Segment{T}, b::Style) where {T} = Node{T}(a,b)
+
 @inline Base.eltype(::Node{T}) where {T} = T
 @inline Base.eltype(::Type{Node{T}}) where {T} = T
 
@@ -184,16 +182,35 @@ Return the style associated with node `x`.
 style(x::Node) = x.sty
 
 """
-    setsegment!(x::Node, s::Segment)
-Set the segment associated with node `x` to `s`.
+    setsegment!(n::Node, s::Segment)
+Set the segment associated with node `n` to `s`. If `reconcile`, then modify fields as
+appropriate for internal consistency (possibly including other linked nodes).
 """
-setsegment!(x::Node, s::Segment) = x.seg = s
+function setsegment!(n::Node, s::Segment; reconcile=true)
+    n.seg = s
+    if reconcile
+        reconcilefields!(n)
+        reconcilestart!(n)
+        n′ = n
+        while next(n′) !== n′
+            n′ = next(n′)
+            reconcilefields!(n′)
+            reconcilestart!(n′)
+        end
+    end
+    return n
+end
 
 """
-    setstyle!(x::Node, s::Style)
-Set the style associated with node `x` to `s`.
+    setstyle!(n::Node, s::Style; reconcile=true)
+Set the style associated with node `n` to `s`. If `reconcile`, then modify fields as
+appropriate for internal consistency.
 """
-setstyle!(x::Node, s::Style) = x.sty = s
+function setstyle!(n::Node, s::Style; reconcile=true)
+    n.sty = s
+    reconcile && reconcilefields!(n)
+    return n
+end
 
 function deepcopy_internal(x::Style, stackdict::IdDict)
     if haskey(stackdict, x)
@@ -242,13 +259,13 @@ function direction(s, t)
 end
 
 """
-    p0{T}(s::Segment{T})
+    p0(s::Segment{T}) where {T}
 Return the first point in a segment (calculated).
 """
 p0(s::Segment{T}) where {T} = s(zero(T))::Point{T}
 
 """
-    p1{T}(s::Segment{T})
+    p1(s::Segment{T}) where {T}
 Return the last point in a segment (calculated).
 """
 p1(s::Segment{T}) where {T} = s(pathlength(s))::Point{T}
@@ -321,8 +338,8 @@ end
 Path(p0::Point=Point(0.0,0.0); α0=0.0)
 Path(p0x::Real, p0y::Real; kwargs...)
 
-Path{T<:Length}(p0::Point{T}; α0=0.0)
-Path{T<:Length}(p0x::T, p0y::T; kwargs...)
+Path(p0::Point{T}; α0=0.0) where {T<:Length}
+Path(p0x::T, p0y::T; kwargs...) where {T<:Length}
 Path(p0x::Length, p0y::Length; kwargs...)
 
 Path(u::LengthUnits; α0=0.0)
@@ -350,8 +367,8 @@ Path(x::Coordinate, y::Coordinate; kwargs...) = throw(DimensionError(x,y))
 
 """
     pathlength(p::Path)
-    pathlength{T}(array::AbstractArray{Node{T}})
-    pathlength{T<:Segment}(array::AbstractArray{T})
+    pathlength(array::AbstractArray{Node{T}}) where {T}
+    pathlength(array::AbstractArray{T}) where {T<:Segment}
     pathlength(node::Node)
 Physical length of a path. Note that `length` will return the number of
 segments in a path, not the physical length of the path.
@@ -442,10 +459,10 @@ include("segments/corner.jl")
 include("segments/compound.jl")
 
 """
-    discretestyle1{T}(p::Path{T})
+    discretestyle1(p::Path)
 Returns the last-used discrete style in the path.
 """
-discretestyle1(p::Path{T}) where {T} = style1(p, DiscreteStyle)
+discretestyle1(p::Path) = style1(p, DiscreteStyle)
 
 """
     contstyle1(p::Path)
@@ -485,18 +502,22 @@ Segments or styles can have fields that depend on the properties of neighbors. E
 This function reconciles node `n` for consistency with neighbors in this regard.
 """
 function reconcilefields!(n::Node)
-    seg = segment(n)
+    seg,sty = segment(n), style(n)
     if isa(seg, Corner)
         seg.extent = extent(style(previous(n)), 1.0)
+    end
+    if isa(sty, ContinuousStyle{true})
+        n.sty = typeof(sty)(
+            (getfield(sty, i) for i in 1:(nfields(sty)-1))..., pathlength(seg))
     end
 end
 
 """
-    reconcilestart!(n::Node, α0, p0)
+    reconcilestart!(n::Node{T}, α0=0, p0=Point(zero(T), zero(T))) where {T}
 This function reconciles the starting position and angle of the segment at path node `n`
 to match the ending position and angle of the previous node.
 """
-function reconcilestart!(n::Node, α0, p0)
+function reconcilestart!(n::Node{T}, α0=0, p0=Point(zero(T), zero(T))) where {T}
     if previous(n) == n # first node
         setα0p0!(segment(n), α0, p0)
     else
@@ -516,7 +537,7 @@ function reconcile!(p::Path, n::Integer=1)
     for j in n:lastindex(p)
         reconcilelinkedlist!(p,j)
         reconcilefields!(p[j])
-        reconcilestart!(p[j], p.α0, p.p0)
+        reconcilestart!(p[j], α0(p), p0(p))
     end
 end
 

src/render/paths.jl
@@ -1,5 +1,5 @@
 """
-    render!{T}(c::Cell, p::Path{T}, meta::Meta=GDSMeta(); kwargs...)
+    render!(c::Cell, p::Path{T}, meta::Meta=GDSMeta(); kwargs...) where {T}
 Render a path `p` to a cell `c`.
 """
 function render!(c::Cell, p::Path{T}, meta::Meta=GDSMeta(); kwargs...) where {T}
@@ -59,7 +59,7 @@ function handle_generic_tapers!(p)
         tapernode = p[i]
         prevnode = previous(tapernode)
         nextnode = next(tapernode)
-        if prevnode === tapernode || nextnode === tapernode
+        if (prevnode === tapernode) || (nextnode === tapernode)
             error("A generic taper cannot start or finish a path")
         end
         taper_style = get_taper_style(prevnode, nextnode)
@@ -72,6 +72,7 @@ end
 function get_taper_style(prevnode, nextnode)
     prevstyle = style(prevnode)
     nextstyle = style(nextnode)
+    beginof_next = zero(pathlength(segment(nextnode)))
     endof_prev = pathlength(segment(prevnode))
     # handle case of compound style (#39)
     if prevstyle isa Paths.CompoundStyle
@@ -80,15 +81,7 @@ function get_taper_style(prevnode, nextnode)
     if nextstyle isa Paths.CompoundStyle
         nextstyle = first(nextstyle.styles)
     end
-    # handle special case of tapers, set endof_prev to normalized dimensionless parameter
-    if prevstyle isa Paths.TaperTrace || prevstyle isa Paths.TaperCPW
-        endof_prev = endof_prev/endof_prev
-    end
-    beginof_next = zero(pathlength(segment(nextnode)))
-    # handle special case of tapers, set beginof_next to normalized dimensionless parameter
-    if nextstyle isa Paths.TaperTrace || nextstyle isa Paths.TaperCPW
-        beginof_next = beginof_next/pathlength(segment(nextnode))
-    end
+
     if ((prevstyle isa Paths.CPW || prevstyle isa Paths.Trace)
         && nextstyle isa Paths.CPW || nextstyle isa Paths.Trace)
         #special case: both ends are Traces, make a Paths.TaperTrace

src/paths/contstyles/compound.jl
@@ -9,7 +9,7 @@ Combines styles together, typically for use with a [`CompoundSegment`](@ref).
 by the outer constructor.
 - `grid`: An array of `t` values needed for rendering the parameteric path.
 """
-struct CompoundStyle{T<:FloatCoordinate} <: ContinuousStyle
+struct CompoundStyle{T<:FloatCoordinate} <: ContinuousStyle{false}
     styles::Vector{Style}
     grid::Vector{T}
 end

src/paths/contstyles/cpw.jl
@@ -1,14 +1,14 @@
-abstract type CPW <: ContinuousStyle end
+abstract type CPW{T} <: ContinuousStyle{T} end
 
 """
-    struct GeneralCPW{S,T} <: CPW
+    struct GeneralCPW{S,T} <: CPW{false}
         trace::S
         gap::T
     end
 A CPW with variable trace and gap as a function of path length. `trace` and `gap` are
 callable.
 """
-struct GeneralCPW{S,T} <: CPW
+struct GeneralCPW{S,T} <: CPW{false}
     trace::S
     gap::T
 end
@@ -24,7 +24,7 @@ copy(x::GeneralCPW) = GeneralCPW(x.trace, x.gap)
     end
 A CPW with fixed trace and gap as a function of path length.
 """
-struct SimpleCPW{T<:Coordinate} <: CPW
+struct SimpleCPW{T<:Coordinate} <: CPW{false}
     trace::T
     gap::T
 end

src/paths/contstyles/decorated.jl
@@ -1,5 +1,5 @@
 """
-    mutable struct DecoratedStyle{T<:FloatCoordinate} <: ContinuousStyle
+    mutable struct DecoratedStyle{T<:FloatCoordinate} <: ContinuousStyle{false}
         s::Style
         ts::Array{Float64,1}
         dirs::Array{Int,1}
@@ -7,7 +7,7 @@
     end
 Style with decorations, like structures periodically repeated along the path, etc.
 """
-mutable struct DecoratedStyle{T<:FloatCoordinate} <: ContinuousStyle
+mutable struct DecoratedStyle{T<:FloatCoordinate} <: ContinuousStyle{false}
     s::Style
     ts::Array{T,1}
     dirs::Array{Int,1}

src/paths/contstyles/strands.jl
@@ -1,7 +1,7 @@
-abstract type Strands <: ContinuousStyle end
+abstract type Strands{T} <: ContinuousStyle{T} end
 
 """
-    struct GeneralStrands{S,T,U} <: Strands
+    struct GeneralStrands{S,T,U} <: Strands{false}
         offset::S
         width::T
         spacing::U
@@ -19,7 +19,7 @@ abstract type Strands <: ContinuousStyle end
 Strands with variable center offset, width, and spacing as a function of path length.
 `offset`, `width`, and `spacing` are callable.
 """
-struct GeneralStrands{S,T,U} <: Strands
+struct GeneralStrands{S,T,U} <: Strands{false}
     offset::S
     width::T
     spacing::U
@@ -33,7 +33,7 @@ copy(x::GeneralStrands) = GeneralStrands(x.offset, x.width, x.spacing, x.num)
 @inline num(s::GeneralStrands, t) = s.num
 
 """
-    struct SimpleStrands{T<:Coordinate} <: Strands
+    struct SimpleStrands{T<:Coordinate} <: Strands{false}
         offset::T
         width::T
         spacing::T
@@ -50,7 +50,7 @@ copy(x::GeneralStrands) = GeneralStrands(x.offset, x.width, x.spacing, x.num)
 
 Strands with fixed center offset, width, and spacing as a function of path length.
 """
-struct SimpleStrands{T<:Coordinate} <: Strands
+struct SimpleStrands{T<:Coordinate} <: Strands{false}
     offset::T
     width::T
     spacing::T

src/paths/contstyles/tapers.jl
@@ -1,49 +1,59 @@
 """
-    struct TaperTrace{T<:Coordinate} <: Trace
+    struct TaperTrace{T<:Coordinate} <: Trace{true}
         width_start::T
         width_end::T
+        length::T
     end
 A single trace with a linearly tapered width as a function of path length.
 """
-struct TaperTrace{T<:Coordinate} <: Trace
+struct TaperTrace{T<:Coordinate} <: Trace{true}
     width_start::T
     width_end::T
+    length::T
+    TaperTrace{T}(ws::T, we::T) where {T<:Coordinate} = new{T}(ws, we)
+    TaperTrace{T}(ws::T, we::T, l) where {T<:Coordinate} = new{T}(ws, we, l)
 end
 copy(x::TaperTrace) = TaperTrace(x.width_start, x_width_end)
-@inline extent(s::TaperTrace, t) = s.width_start/2  + (s.width_end - s.width_start)*t/2
-@inline width(s::TaperTrace, t) = s.width_start + (s.width_end - s.width_start)*t
+@inline extent(s::TaperTrace, t) = 0.5 * width(s,t)
+@inline width(s::TaperTrace, t) = (1-t/s.length) * s.width_start + t/s.length * s.width_end
 function TaperTrace(width_start::Coordinate, width_end::Coordinate)
     dimension(width_start) != dimension(width_end) && throw(DimensionError(trace,gap))
     w_s,w_e = promote(float(width_start), float(width_end))
-    TaperTrace(w_s, w_e)
+    return TaperTrace{typeof(w_s)}(w_s, w_e)
 end
 
 """
-    struct TaperCPW{T<:Coordinate} <: CPW
+    struct TaperCPW{T<:Coordinate} <: CPW{true}
         trace_start::T
         gap_start::T
         trace_end::T
         gap_end::T
+        length::T
     end
 A CPW with a linearly tapered trace and gap as a function of path length.
 """
-struct TaperCPW{T<:Coordinate} <: CPW
+struct TaperCPW{T<:Coordinate} <: CPW{true}
     trace_start::T
     gap_start::T
     trace_end::T
     gap_end::T
+    length::T
+    TaperCPW{T}(ts::T, gs::T, te::T, ge::T) where {T<:Coordinate} = new{T}(ts, gs, te, ge)
+    TaperCPW{T}(ts::T, gs::T, te::T, ge::T, l) where {T<:Coordinate} =
+        new{T}(ts, gs, te, ge, l)
 end
 copy(x::TaperCPW) = TaperCPW(x.trace_start, x.gap_start, x.trace_end, x.gap_end)
-@inline extent(s::TaperCPW, t) = s.trace_start/2 + s.gap_start + (s.trace_end/2-s.trace_start/2 + s.gap_end-s.gap_start)*t
-@inline trace(s::TaperCPW, t) = s.trace_start + (s.trace_end-s.trace_start)*t
-@inline gap(s::TaperCPW, t) = s.gap_start + (s.gap_end-s.gap_start)*t
+@inline extent(s::TaperCPW, t) = (1-t/s.length) * (0.5*s.trace_start + s.gap_start) +
+    (t/s.length) * (0.5*s.trace_end + s.gap_end)
+@inline trace(s::TaperCPW, t) = (1-t/s.length) * s.trace_start + t/s.length * s.trace_end
+@inline gap(s::TaperCPW, t) = (1-t/s.length) * s.gap_start + t/s.length * s.gap_end
 function TaperCPW(trace_start::Coordinate, gap_start::Coordinate, trace_end::Coordinate, gap_end::Coordinate)
     ((dimension(trace_start) != dimension(gap_start)
         || dimension(trace_end) != dimension(gap_end)
         || dimension(trace_start) != dimension(trace_end))
         && throw(DimensionError(trace,gap)))
     t_s,g_s,t_e,g_e = promote(float(trace_start), float(gap_start), float(trace_end), float(gap_end))
-    TaperCPW(t_s, g_s, t_e, g_e)
+    return TaperCPW{typeof(t_s)}(t_s, g_s, t_e, g_e)
 end
 
 summary(s::TaperTrace) = string("Tapered trace with initial width ", s.width_start,
@@ -53,17 +63,12 @@ summary(s::TaperCPW) = string("Tapered CPW with initial width ", s.trace_start,
                                " tapers to a final width ", s.trace_end,
                                " and final gap ", s.gap_end)
 
-struct Taper <: ContinuousStyle end
-copy(::Taper) = Taper()
-
 """
     Taper()
-Constructor for generic Taper style. Will automatically create a linearly
-tapered region between an initial `CPW` or `Trace` and an end `CPW` or `Trace`
-of different dimensions. 
+Constructor for generic Taper style. Will automatically create a linearly tapered region
+between an initial `CPW` or `Trace` and an end `CPW` or `Trace` of different dimensions.
 """
-Taper
+struct Taper <: ContinuousStyle{false} end
+copy(::Taper) = Taper()
 
-summary(::Taper) = string("Generic tapered region constructing a linear taper ",
-                          "between the segment before and segment after its ",
-                          "place in the path")
+summary(::Taper) = string("Generic linear taper between neighboring segments in a path")

src/paths/contstyles/trace.jl
@@ -1,12 +1,12 @@
-abstract type Trace <: ContinuousStyle end
+abstract type Trace{T} <: ContinuousStyle{T} end
 
 """
-    struct GeneralTrace{T} <: Trace
+    struct GeneralTrace{T} <: Trace{false}
         width::T
     end
 A single trace with variable width as a function of path length. `width` is callable.
 """
-struct GeneralTrace{T} <: Trace
+struct GeneralTrace{T} <: Trace{false}
     width::T
 end
 copy(x::GeneralTrace) = GeneralTrace(x.width)
@@ -14,12 +14,12 @@ copy(x::GeneralTrace) = GeneralTrace(x.width)
 @inline width(s::GeneralTrace, t) = s.width(t)
 
 """
-    struct SimpleTrace{T<:Coordinate} <: Trace
+    struct SimpleTrace{T<:Coordinate} <: Trace{false}
         width::T
     end
 A single trace with fixed width as a function of path length.
 """
-struct SimpleTrace{T<:Coordinate} <: Trace
+struct SimpleTrace{T<:Coordinate} <: Trace{false}
     width::T
 end
 SimpleTrace(width) = SimpleTrace(width)

commit 1c9c515cb9e1279442240e06d0dde474dab28d7c

Fix bug introduced in b084d38.

src/paths/paths.jl
@@ -190,7 +190,7 @@ function setsegment!(n::Node, s::Segment; reconcile=true)
     n.seg = s
     if reconcile
         reconcilefields!(n)
-        reconcilestart!(n)
+        reconcilestart!(n, α0(s), p0(s))
         n′ = n
         while next(n′) !== n′
             n′ = next(n′)

test/runtests.jl
@@ -664,6 +664,12 @@ end
         @test ForwardDiff.derivative(segment(pa[2]), (5π/2)μm) ≈ Point(0.0, 1.0)
         @test Devices.Paths.curvature(segment(pa[2]), 0.0μm) ≈ Point(0.0/μm, 0.2/μm)
         @test Devices.Paths.curvature(segment(pa[2]), (5π/2)μm) ≈ Point(-0.2/μm, 0.0/μm)
+
+        # test setsegment! works correctly
+        pa = Path(μm, α0=90°)
+        straight!(pa, 100μm, Paths.Trace(2μm))
+        setsegment!(pa[1], copy(segment(pa[1])))
+        @test α0(pa) == 90°
     end
 
     @testset "> Path-based launchers" begin