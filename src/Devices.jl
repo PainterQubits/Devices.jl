c24495281abfa0b3f3d55b007625b934d46a7685
Andrew Keller <andrew.keller.09@gmail.com>
2019-03-29 15:00:28 -0700

    Strip out graphical interface because of compatibility concerns; it can be reimplemented later, or in a separate package.

--- src/Devices.jl
+++ src/Devices.jl
@@ -3,7 +3,6 @@ using Random
 using LinearAlgebra
 using ForwardDiff
 using FileIO
-using WebIO
 include("units.jl")
 
 import StaticArrays
@@ -21,7 +20,6 @@ export datatype, layer, render!
 
 const DEFAULT_LAYER = 0
 const DEFAULT_DATATYPE = 0
-const D3STR = read(joinpath(dirname(@__FILE__), "../deps/d3.min.js"), String)
 
 # setup for robust 2d predicates
 const splitter, epsilon =
@@ -288,162 +286,5 @@ export LCDFonts,
     lcdstring!,
     referenced_characters_demo,
     scripted_demo
-
-function Base.show(io::IO, mime::MIME"application/juno+plotpane", c0::Cell)
-    svgio = IOBuffer()
-    ps = get(io, :juno_plotsize, [100, 100])
-    bx,by,un,yoff,xoff = let b=bounds(c0)
-        ustrip(width(b)), ustrip(height(b)), unit(width(b)), ustrip(b.ur.y), ustrip(b.ll.x)
-    end
-    sx,sy = bx/ps[1]*96/72, by/ps[2]*96/72
-    maxs = max(sx,sy)
-    show(svgio, MIME"image/svg+xml"(), c0; width=(ps[1]/96)inch, height=(ps[2]/96)inch, bboxes=true)
-    svgstr = String(take!(svgio))
-    svgstr = replace(svgstr, r"<\?[A-Za-z .=0-9\-\"]+\?>"=>"")
-    w = Scope()
-    show(io, mime, w(dom"div"(
-        dom"script"(setInnerHtml=D3STR),
-        dom"div"(setInnerHtml=svgstr),
-        dom"script"("""
-            firstPoint = false;                 // keeps track of first vs. second click
-            firstPointLoc = {x: 0, y: 0};       // keeps track of first click location
-            translateVar =  {x: 0, y: 0};       // keeps track of zooming
-            scaleVar = 1;
-
-            // go from mouse click coordinate to cell coordinate
-            function rescl(datum, scale) {
-                return {x: (datum.x - translateVar.x) / scaleVar * scale + $xoff,
-                        y: (datum.y - translateVar.y) / -scaleVar * scale + $yoff};
-            }
-
-            function updateLine(g, data) {
-                if (firstPoint) {
-                    d = g.selectAll("line").data(data)
-                    d.enter()
-                     .append("line")
-                     .merge(d)
-                     .attr("x1", (firstPointLoc.x - $xoff) * scaleVar + translateVar.x)
-                     .attr("y1", (firstPointLoc.y - $yoff) * -scaleVar + translateVar.y)
-                     .attr("x2", function(d) {return d.x})
-                     .attr("y2", function(d) {return d.y})
-                     .attr("stroke", "red")
-
-                     d = g.selectAll("#dist").data(data)
-                     d.enter()
-                      .append("text")
-                      .attr("id", "dist")
-                      .merge(d)
-                      .attr("fill", "red")
-                      .attr("font-weight", "bold")
-                      .attr("font-family", "sans-serif")
-                      .attr("x", function(d) {return d.x})
-                      .attr("y", function(d) {return d.y})
-                      .text(function (d) {
-                          var p1 = {x: (firstPointLoc.x - $xoff) * $maxs + $xoff,
-                                    y: (firstPointLoc.y - $yoff) * $maxs + $yoff}
-                          var p2 = rescl(d, $maxs)
-                          var d = Math.sqrt(Math.pow(p1.x - p2.x, 2) +
-                            Math.pow(p1.y - p2.y, 2))
-                          return d.toFixed(3) + " $un"
-                      })
-                } else {
-                    // user clicked a second time, remove the measurement line.
-                    g.selectAll("line").remove()
-                    g.selectAll("#dist").remove()
-                }
-            }
-
-            // fill the plot pane with the svg
-            var svg = d3.selectAll("svg")
-                    .attr("width", "100%")
-                    .attr("height", "100%")
-            var oldg = svg.selectAll("g")
-
-            var newg = svg.append("g")
-            newg.append("rect")                    // white rect in upper-left for text.
-                .attr("fill", "white")
-                .attr("opacity", "0.5")
-                .attr("width", "300px")
-                .attr("height", "18px")
-            newg.append("rect")                    // capture all mouse events in plot pane.
-                .attr("fill", "none")
-                .attr("pointer-events", "all")
-                .attr("cursor", "crosshair")
-                .attr("width", "100%")
-                .attr("height", "100%")
-
-            d3.select("body").on("keydown", function() {
-                var k = d3.event.key;
-                if (k == '/') {
-                    newg.call(d3.zoom().transform, d3.zoomIdentity)
-                    oldg.attr("transform", d3.zoomIdentity)
-                    translateVar = {x:0, y:0}
-                    scaleVar = 1
-                    updateLine(newg, data)
-                }
-            })
-
-            newg.on("mousemove", function() {
-                var coords = d3.mouse(this);
-                data = [{
-                    x: coords[0],
-                    y: coords[1]
-                }];
-
-                var d = newg.selectAll("text").data(data)
-                d.enter()
-                 .append("text")
-                 .attr("x", function() { return 0; })
-                 .attr("y", function() { return 10; })
-                 .attr("font-family", "Consolas", "monospace")
-                 .attr("font-size", 14)
-                 .merge(d)
-                 .text(function(d) {
-                    e = rescl(d, $maxs)
-                    return "X: " + e.x.toFixed(3).padStart(10, " ") + " $un" +
-                           "  Y: " + e.y.toFixed(3).padStart(10, " ") + " $un";
-                 })
-
-                 updateLine(newg, data)
-                 newg.attr("cursor", "crosshair")
-            })
-
-            newg.on("click", function() {
-                if (firstPoint) {
-                    firstPoint = false;
-                } else {
-                    firstPoint = true;
-                    var coords = d3.mouse(this)
-                    coords = rescl({x: coords[0], y: coords[1]}, 1)
-                    firstPointLoc = {x: coords.x, y: coords.y};
-                }
-
-                // Display or hide text and line after click as appropriate.
-                var d = newg.selectAll("text").data(data)
-                updateLine(newg, data)
-            })
-
-            newg.call(d3.zoom()
-                .scaleExtent([0.0001, 10000])
-                .on("zoom", function () {
-                    var coords = d3.mouse(this);
-                    data = [{
-                        x: coords[0],
-                        y: coords[1]
-                    }];
-                    translateVar.x = d3.event.transform.x;
-                    translateVar.y = d3.event.transform.y;
-                    scaleVar = d3.event.transform.k;
-                    oldg.attr("transform", d3.event.transform)
-                    updateLine(newg, data)
-                }));
-
-            newg.on("dblclick.zoom", null)
-        """
-        )
-    )))
-    show(stdout, MIME"text/plain"(), c0)
-    println(stdout)
-end
 
 end
